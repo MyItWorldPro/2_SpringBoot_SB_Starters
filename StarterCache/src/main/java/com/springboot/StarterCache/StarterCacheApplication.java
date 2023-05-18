9740fab307483945e25817e30dad1e6ba1089898
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2023-05-18 19:54:48 +0530

    initial commit

---
+++ StarterCache/src/main/java/com/springboot/StarterCache/StarterCacheApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.StarterCache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterCacheApplication.class, args);
+	}
+
+}
---
+++ StarterCache/src/test/java/com/springboot/StarterCache/StarterCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.StarterCache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
